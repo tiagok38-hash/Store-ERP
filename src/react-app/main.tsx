c474135441882a905009d455750f13428b87fc0d
[dyad] <git@dyad.sh>
2025-09-25 22:38:20 -0300

    [dyad] Corrigindo erro de contexto do Router em main.tsx - wrote 1 file(s)

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Importar BrowserRouter
 import "@/react-app/index.css";
 import App from "@/react-app/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> {/* Envolver App com BrowserRouter */}
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
